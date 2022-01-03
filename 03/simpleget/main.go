02d5f3775685896d293194745826ec6cbaf6f31c
Msksgm <msksgm3124@gmail.com>
2022-01-03 15:21:54 +0900

    add: p.87

--- 03/simpleget/main.go
+++ 03/simpleget/main.go
@@ -4,14 +4,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 )
 
 func main() {
-	values := url.Values{
-		"query": {"hello world"},
-	}
-	resp, err := http.Get("http://localhost:18888" + "?" + values.Encode())
+	// values := url.Values{
+	// 	"query": {"hello world"},
+	// }
+	// resp, err := http.Get("http://localhost:18888" + "?" + values.Encode())
+	resp, err := http.Head("http://localhost:18888")
 	if err != nil {
 		panic(err)
 	}
@@ -23,7 +23,7 @@ func main() {
 	}
 	log.Println(string(body))
 	log.Println("Status:", resp.Status)
-	log.Println("StatusCode:", resp.StatusCode)
-	log.Println("Header:", resp.Header)
-	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
+	// log.Println("StatusCode:", resp.StatusCode)
+	log.Println("Headers:", resp.Header)
+	// log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 }
