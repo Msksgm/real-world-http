1df362b5fa397e8ce2adad6d9d83d85f4d8ee9ac
Msksgm <msksgm3124@gmail.com>
2022-01-03 15:23:48 +0900

    add: p.88

--- 03/simpleget/main.go
+++ 03/simpleget/main.go
@@ -1,29 +1,20 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	// values := url.Values{
-	// 	"query": {"hello world"},
-	// }
-	// resp, err := http.Get("http://localhost:18888" + "?" + values.Encode())
-	resp, err := http.Head("http://localhost:18888")
+	values := url.Values{
+		"test": {"value"},
+	}
+	resp, err := http.PostForm("http://localhost:18888", values)
 	if err != nil {
+		// 送信失敗
 		panic(err)
 	}
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(string(body))
 	log.Println("Status:", resp.Status)
-	// log.Println("StatusCode:", resp.StatusCode)
-	log.Println("Headers:", resp.Header)
-	// log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 }
